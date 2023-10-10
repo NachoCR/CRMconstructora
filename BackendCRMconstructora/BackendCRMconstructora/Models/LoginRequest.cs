97e6bf908b79ab35171e5b0ed013244c585152a8
georgeule-cloud <82237911+georgeule-cloud@users.noreply.github.com>
2023-10-10 13:33:39 -0600

    Refactor folders

--- BackendCRMconstructora/BackendCRMconstructora/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BackendCRMconstructora.Models
-{
-    public class LoginRequest
-    {
-        public string Identifier { get; set; }
-        public string Password { get; set; }
-    }
-}
